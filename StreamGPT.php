eaca4a174bc0ddd4e8732f7fdc636de875984080
CodeChap <hello@codechap.com>
2023-05-07 17:00:22 +0200

    StreamGPT

--- StreamGPT.php
+++ StreamGPT.php
@@ -1,10 +1,5 @@
-# CodeChirps
+<?php
 
-Introducing "CodeChirps" - bite-sized code snippets for every digital nest builder out there, ready to help you soar through your coding journey! 🐦💻 #CodeChirps #FlyWithCode
-
-### StreamGPT
-If you want to 😎 steam from OpenAI just like the big kids do🚀, here is how to do it with PHP from the command line 💻. #chatGPT4 #OpenAi #php #CodeChirps 🐦🤖✨
-```php
 // To run, type: php StreamGPT.php
 
 // Set API key and endpoint
@@ -61,33 +56,4 @@ curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($curl, $data) {
 });
 
 curl_exec($ch);
-```
-
-### GiggleSearch
-If you ever feel the need to reinvent the wheel and build your own  search engine for giggles, don't sweat it! Just use Google's API and  call it "GiggleSearch"! Voila! 😂🔍 Here's how: #Googleception  #RecursionFTW #buildinpublic 
-
-```php
-// Google search endpoint
-$endPoint = "https://www.googleapis.com/customsearch/v1";
-
-// Set credentials
-$apiKey  = file_get_contents('googleSearchApiKey.secret'); // Obtained from https://console.developers.google.com/apis/credentials
-$cx      = file_get_contents('searchEngineId.secret'); // Obtained from https://cse.google.com/cse/all
-
-// Set parameters ( Additional parameters can be found here: https://developers.google.com/custom-search/json-api/v1/reference/cse/list)
-$start   = 1; // Start index of the results
-$num     = 10; // Number of results per page, max is 10
-$country = 'ZA'; // Country code to search from
-
-// Set the query to search for in google
-$query = urlencode("Open AI");
-
-// Build the final URL
-$url = $endPoint."?key={$apiKey}&cx={$cx}&gl={$country}&start={$start}&num={$num}&q={$query}";
-
-// Get the results from google
-$result = json_decode(file_get_contents($url));
-
-// Dump the result
-print_r($result);
-```
+?>
